ad83ae8c7a6258b8ff7bf23bbd5e192cff62be70
Mohak Bansal <mohak64bansal@gmail.com>
2023-02-16 17:46:56 +0530

    complete #24

--- src/components/News.js
+++ src/components/News.js
@@ -4,13 +4,24 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        this is an news component
-        <NewsItem/>
-        <NewsItem/>     
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container' my-3>
+        <h2>NewsMonkey - Top Headlines</h2>
+        {/* this is an news component */}
+
+        <div className='row'>
+            <div className="col-md-4">
+            <NewsItem  title="mytitle" description="jodmohak"/>
+            </div>
+
+            <div className="col-md-4">
+            <NewsItem  title="mytitle" description="jodmohak"/>
+            </div>
+
+            <div className="col-md-4">
+            <NewsItem  title="mytitle" description="jodmohak"/>
+            </div>
+        </div>
+        
         
         
       </div>
