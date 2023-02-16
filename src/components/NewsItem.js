f57059c229d44af466fb38c7d404e0a5a1e3db67
Mohak Bansal <mohak64bansal@gmail.com>
2023-02-17 01:28:37 +0530

    complete #27

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,10 +6,18 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "25rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              !imageUrl
+                ? "https://static.toiimg.com/thumb/msid-47529300,width-1070,height-580,imgsize-110164,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
             <a
               href={newsUrl}
               target="_blank"
