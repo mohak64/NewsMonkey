62892f61d53e71f85f8ddd6da74695ff55c73fcc
Mohak Bansal <mohak64bansal@gmail.com>
2023-02-17 00:26:33 +0530

    #25 done

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
-
+//rce
 export class NewsItem extends Component {
+  
+
   render() {
-    let {title, description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem;"}}>
-            <img src="..." className="card-img-top" alt="..."/>
+            <img src={imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+                <a href="/newsdetail/" className="btn btn-sm btn-primary">Read More</a> 
+                {/* btn-sm se button chota */}
             </div>
         </div>
       </div>
